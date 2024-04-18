3da8b109fb30eb858acee1f89136c928d3328c56
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2024-04-19 00:25:07 +0300

    some styling fixes

--- src/Search.jsx
+++ src/Search.jsx
@@ -4,18 +4,18 @@ function Search() {
   const articles = [
     {
       id: 1,
-      title: "First article",
-      description: "first first one",
+      title: "How do you write an article review in APA format?",
+      description: "To write an article review in APA format, start by formatting the citation of the article. Read through the article and identify the standard APA sections, such as the abstract, in...",
     },
     {
       id: 2,
-      title: "Second article",
-      description: "second second one",
+      title: "Where can I read the national enquirer online?",
+      description: "The National Enquirer has an online version, found at NationalEnquirer.com, that features articles from the publication. The site also has a subscription option for the digital edi...",
     },
     {
       id: 3,
-      title: "Third article",
-      description: "third third one",
+      title: "What is the reading level of the New York Times?",
+      description: "The New York Times in School program states that the newspaper’s reading level varies by article. It urges teachers using the Times as an educational tool to choose articles for st...",
     },
   ];
 
@@ -30,11 +30,12 @@ function Search() {
   };
 
   return (
-    <div>
-      <h2>Search</h2>
+    <div className="container">
+      <h1>Search</h1>
       <input
+        className="search-input"
         type="text"
-        placeholder="type to search..."
+        placeholder="Type to search..."
         value={searchWord}
         onChange={handleOnChange}
       />
